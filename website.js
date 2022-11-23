33cb817b87f4e23e03782d28c22ac1a5a5359a84
eliazzo <bethazz@hotmail.co.uk>
2022-11-23 13:14:29 +0000

    simplified image carousel feature

--- website.js
+++ website.js
@@ -1,3 +1,5 @@
+// Website text
+
 document.getElementById("nationality-description").innerText = "My grandparents are first generation immigrants and moved to London in 19XX from Malta where my Dad was born. My mother is daughter to an American and an Englishman who met in the Congo in 1962. I was born and raised in Highbury, London.";
 document.getElementById("degree-description").innerText = "I graduated in July 2022 with first class honours from the Unviversity of Manchester. My degree was evenly split between Spanish, which involved the study of hispanic literature, film, history and politics, alongisde the language. And, Linguistics, which I studied from sociological, philosophical and psychological perspecives.";
 document.getElementById("job-description").innerText = "I'm currently working at a secondary school in Hackney. My role is divided between the English department and the SEN department. I tutor small groups of GCSE students with a particular focus on their English Language GCSE. I also support students with additional learning needs, such as Autism Spectrum Disorder. The school has many students who speak English as a second language and my role includes providing support to some of the Spanish speaking students.";
@@ -7,3 +9,30 @@ document.getElementById("language-text").innerText = "I have a passion for langu
 document.getElementById("translation-example").innerText = "Harriet gave the parcel to Donald. \n GIVE (h, the-parcel, d) \n 𝛌x [Harriet gave x to Donald] \n 𝛌x [GIVE (h, x, d)] \n \n Let h = Harriet, d = Donald and x = the-parcel \n In this example x was 𝛌-abstracted";
 document.getElementById("language-text-continued").innerText = "It won't lie to you, it was challenging. But I threw myself into it and eventually found my feet. I suppose it's given me the confidence to pursue a career in coding."
 document.getElementById("learning-text").innerText = "As the above text hopefully demonstrates, I loved being an undergraduate. Learning brings me such joy and if I had unlimited amounts of money I could happily spend the rest of my life moving from degree to degree. I often thought about (and still think about) applying for a masters. However, while I am thrilled to have the skills to analyse and critique the use of the Supernatural in Latin American fiction, or translate English lexemes (among other, obscure skills), I am hesitant to pursue more education without a definitive end goal. That's where Founders and Coders come in! Since joining the FAC community I have rediscovered a community where peer learning and support is central. I am delighted with the prospect of a career that involves continuously researching and learning. Joining FAC would enable me to continute my education, learn a multitude of new skills and provide me with an exciting career path - all of which I am desperately searching for!";
+
+// Yoga carousel
+
+let yogaList = document.getElementById("yoga-list");
+const yogaListItems = document.querySelectorAll("li");
+const numberOfYogaListItems = yogaListItems.length;
+
+function imageChange0(){
+    yogaList.style.left = ("0px"); // Sets the css left property to 0px
+    imagePosition = 0;
+}
+
+function imageChange1(){
+    yogaList.style.left = ("-400px"); // Sets the css left property to -400px
+    imagePosition = 1;
+}
+
+function imageChange2(){
+    yogaList.style.left = ("-800px"); // Sets the css left property to -800px
+    imagePosition = 2;
+}
+
+function imageChange3(){
+    yogaList.style.left = ("-1200px"); // Sets the css left property to -800px
+    imagePosition = 3;
+}
+
